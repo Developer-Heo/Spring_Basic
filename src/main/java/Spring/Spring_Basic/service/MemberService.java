aa334a8e517b67725db58f35d9b39b7c8e223735
Developer-Heo <wook.heo89@gmail.com>
2022-03-02 22:42:41 +0900

    updated on 2nd,Mar,2022

---
+++ src/main/java/Spring/Spring_Basic/service/MemberService.java
@@ -0,0 +1,41 @@
+package Spring.Spring_Basic.service;
+
+import Spring.Spring_Basic.domain.Member;
+import Spring.Spring_Basic.repository.MemberRepository;
+import Spring.Spring_Basic.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository; //= new MemoryMemberRepository();
+
+    public MemberService(MemberRepository memberRepository) { //Generate -> Constructor
+        this.memberRepository = memberRepository;
+    }
+
+    // 회원가입
+
+    public Long join(Member member){
+       // 같은 이름이 있는 중복 회원x - 중복회원 검증
+        validateDupulicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDupulicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {    // 존재하면~ (if not null)
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
